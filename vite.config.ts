7f2159fa304b7720aa94f076e3a94947769c199a
Abdouni Abdelkarim <abdounikarim@gmail.com>
2025-09-29 17:26:18 +0200

    HTTPS with Vite 🚀

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import fs from 'fs'
+import type { ServerOptions } from 'vite'
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
+export default defineConfig(({ mode }) => {
+	const isDev = mode === 'development';
+
+	const serverConfig: ServerOptions = {
 		host: true,
-		port: 8000
-	},
-	preview: {
-		port: 8080,
-	},
+		port: 8000,
+	};
+
+	if (isDev) {
+		serverConfig.https = {
+			key: fs.readFileSync('certs/poc-sveltekit.key.pem'),
+			cert: fs.readFileSync('certs/poc-sveltekit.crt.pem'),
+		};
+	}
+
+	return {
+		plugins: [sveltekit()],
+		server: serverConfig,
+		preview: {
+			port: 8080,
+		},
+	}
 });
